c52abe49b5b93388f2e17faf181490d856815031
SAUL YOUSSEF <youssef@SAULs-MacBook-Pro.local>
2023-04-22 14:03:27 -0400

    ...

---
+++ py/base_new.py
@@ -0,0 +1,13 @@
+#
+#   A foundational system for math and computing
+#
+#   This is the result of years of gradually simplifying previous personal computing systems, particularly
+#   "types", "ee", "egg" (with Margo Seltzer and David Parkes), an attempt to use Aldor for Category theory
+#   (Aldor is by Stephen Watt and collaborators), and "coda-classic".  This system and it's internal language
+#   is also called "coda".
+#
+#   Saul Youssef, January 2023
+#====================================================================================
+#
+#    A coda is a pair of data
+#

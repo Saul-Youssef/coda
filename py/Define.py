e56fa411f179db226c6d806ef803b86476fd9eb6
SAUL YOUSSEF <youssef@SAULs-MacBook-Pro.local>
2023-04-25 10:54:38 -0400

    ...

--- py/Define.py
+++ py/Define.py
@@ -16,15 +16,34 @@ def Def(domain,D,V):
     V = Evaluate.resolve(V,100)
 
     if (not D is None) and (not V is None):
-        if V.empty():
-#            CONTEXT.dom(D,Definition(D))
-            CONTEXT.add(Definition(D))
-        else:
-            defin = Definition(D,lambda domain,A,B:data((V+A)|B))
-#            CONTEXT.dom(defin.domain(),defin)
-            CONTEXT.add(defin)
+        if V.empty(): CONTEXT.add(Definition(D))
+        else        : CONTEXT.add(Definition(D,lambda domain,A,B:data((V+A)|B)))
         return data()
 CONTEXT.define('def',Def)
+
+VALUES = {}
+def Value(domain,A,B):
+    if B.invariant() and B in VALUES: return VALUES[B]
+CONTEXT.define('?',Value)
+def Let(domain,A,B):
+    if A.invariant():
+        VALUES[A] = B
+        return data()
+CONTEXT.define('let',Let)
+
+#def Val(domain,A,B):
+#    if A.invariant() and A in CONTEXT.var(): return CONTEXT.val(A)
+#CONTEXT.define('val',Val)
+#
+#    if (not D is None) and (not V is None):
+#        if V.empty():
+##            CONTEXT.dom(D,Definition(D))
+#            CONTEXT.add(Definition(D))
+#        else:
+#            defin = Definition(D,lambda domain,A,B:data((V+A)|B))
+##            CONTEXT.dom(defin.domain(),defin)
+#            CONTEXT.add(defin)
+#        return data()
 #
 #   Named constant storage
 #

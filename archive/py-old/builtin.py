f67f8b7e7bd24657a46bd58c5b01823d00d494c9
SAUL YOUSSEF <youssef@SAULs-MacBook-Pro.local>
2023-12-23 22:18:48 -0500

    ...

--- archive/py-old/builtin.py
+++
@@ -1,5 +0,0 @@
-#
-#   Import built in definitions
-#
-import Language,Basic,Help,Sequence,Logic,Import, \
-    Define,Apply,Code,Collect,Compile,Text,Sample,IO,Variable,Multi,With
--- archive/py/builtin.py
+++
@@ -1,5 +0,0 @@
-#
-#   Import built in definitions
-#
-import Code,Basic,Language,Apply,Help,Sequence,\
-       Define,Logic,Compile,IO,Import,Text,Path,Collect

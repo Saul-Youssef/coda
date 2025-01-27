a0863b9f62980523268eb3ed3f363bc79fbde85d
SAUL YOUSSEF <youssef@SAULs-Mac-Studio.local>
2025-01-26 23:07:51 -0500

    ...

--- py/Collect.py
+++ py/Collect.py
@@ -10,10 +10,11 @@ from base import *
 #   3. Codes with the same length as byte sequences.
 #   4. Same as 3 with the length (bin L:..codes with length L..)
 #
-#   demo: collect : a b c d
 #   demo: collect : (bin:a) (bin:b) (bin:c) (bin:d)
 #   demo: collect : ap {put bin (nchar:B):B} : a b aa bb aaa cccc zz xxx xxx
 #   demo: collect : (bin 1:c) (bin 1:a b) (bin 2:aa bb zz) (:a) (:b) a b c
+#   demo: sameLeft : (bin:a) (bin:b) (bin:c) (bin:d)
+#   demo: sameRight: (bin 1:a) (bin 2:a) (bin 3:a) (bin 4:b) (bin 5:b)
 #   demo: equiv nchar : a b aa bb aaa cccc zz xxx xxx
 #
 def collect(context,domain,A,B):
@@ -26,4 +27,14 @@ def collect(context,domain,A,B):
         for left,rights in lefts.items(): L.append(left|data(*rights))
         return data(*L)
 CONTEXT.define('collect',collect)
+CONTEXT.define('sameLeft',collect)
+def collectRight(context,domain,A,B):
+    if B.rigid(context):
+        Swap = [(b.domain()+b.right())|b.arg() for b in B]
+        R = collect(context,da('collect'),A,data(*Swap))
+        if not R is None:
+            Swap = [(r.domain()+r.right())|r.arg() for r in R]
+            return data(*Swap)
+CONTEXT.define('sameRight',collectRight)
+
 #    if all([b.atom(context) and b.left().rigid(context) for b in B]):
---
+++ study/Space/refs.md
@@ -0,0 +1,8 @@
+
+[connes](https://www.youtube.com/watch?v=dDm0wxnawJE)
+
+[semiring](https://en.wikipedia.org/wiki/Semiring)
+
+[near semiring](https://en.wikipedia.org/wiki/Near-semiring)
+
+[semiring nlab](https://ncatlab.org/nlab/show/semiring)
